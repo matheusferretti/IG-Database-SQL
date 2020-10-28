22915d866e6f9111320d3c71db83ae24e84d9d6d
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-28 19:42:51 +0000

    updating

--- src/models.py
+++ src/models.py
@@ -9,23 +9,35 @@ from eralchemy import render_er
 Base = declarative_base()
 
 
-class Person(Base):
-    __tablename__ = 'person'
+class ShoppingCart(Base):
+    __tablename__ = 'shoppingcart'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    product_id = Column(Integer, primary_key=True)
+    customer_id = Column(String(250))
+    quantity = Column(Integer(250))
+    price = Column(float(250))
+    bill_id = Column(Integer(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Product(Base):
+    __tablename__ = 'product'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, ForeignKey('shoppingcart.product_id'))
+    shoppingcart = relationship(ShoppingCart)
+    name = Column(String(250))
+    pricing = Column(float(250))
+    weight = Column(float(250), nullable=False)
+
+class Customer(Base):
+    id = Column(Integer, ForeignKey('shoppingcart.customer_id'))
+    shoppingcart = relationship(ShoppingCart)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250))
+    address = Column(String(250))
+
+class Bill(Base):
 
     def to_dict(self):
         return {}
