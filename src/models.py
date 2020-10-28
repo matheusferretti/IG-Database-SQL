67a4ac17b5c45683f0c39713edcd66cb44784e19
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-28 23:13:44 +0000

    updating

--- src/models.py
+++ src/models.py
@@ -19,32 +19,33 @@ class User(Base):
     lastname = Column(String(250), nullable=False, unique=True)
     email = Column(String(250), nullable=False, unique=True)
 
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer(250), primary_key = True, unique=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False, unique=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False, unique=False)
+    user = relationship(User)
+
 class Comment(Base):
     __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer(250), primary_key = True, nullable = True, unique=True)
-    comment_text = Column(String(250), nullable = True, unique=True)
+    comment_text = Column(String(250), nullable = False)
     author_id = Column(String(250), ForeignKey('user.id'), nullable=False)
     post_id = Column(String(250), ForeignKey('post.id'), nullable=False)
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer(250), primary_key = True, nullable = True, unique=True)
-    user_id = Column(Integer(250), nullable=True)
+    id = Column(Integer, primary_key = True, unique=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, unique = False)
 
 class Media(Base):
     __tablename__ = 'media'
-    id = Column(Integer(250), primary_key = True, nullable = True, unique=True)
-    type = enumerate()
-    url = Column(String(250), nullable=True)
-    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
-
-class Followers(Base):
-    __tablename__ = 'followers'
-    id = Column(Integer(250), primary_key = True, nullable = True, unique=True)
-    user_from_id = Column(Integer, ForeignKey('User.id'), nullable=False)
-    user_to_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    id = Column(Integer, primary_key = True, unique=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('user.id'), nullable=False, unique=False)
 
 
 
